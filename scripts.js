f43e8b1f56563b54d693fb798e63bbbf5a2faeca
Clayton Bentes <claytonbentes@live.com>
2025-01-17 23:57:59 -0300

    feat: Implementing BRL converter

---
+++ scripts.js
@@ -0,0 +1,66 @@
+const USD = 6.07
+const EUR = 6.23
+const GBP = 7.38
+
+const form = document.querySelector("form")
+const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
+const result = document.getElementById("result")
+
+// Manipulando o input amount para receber somente numeros
+amount.addEventListener("input", () => {
+    const hasCharactersRegex = /\D+/g
+    amount.value = amount.value.replace(hasCharactersRegex, "")
+})
+
+// captando o evento de submit do formulario
+form.onsubmit = (event) => {
+    event.preventDefault()
+
+    switch(currency.value){
+        case "USD":
+            convertCurrency(amount.value, USD, "US$")
+            break
+        case "EUR":
+            convertCurrency(amount.value, EUR, "€")
+            break
+        case "GBP":
+            convertCurrency(amount.value, GBP, "£")
+            break
+    }
+}
+
+//funcao para converter a moeda.
+
+function convertCurrency(amount, price, symbol){
+    try{
+        description.textContent = ` ${symbol} 1 = ${formatCurrencyBRL(price)}`
+
+        let total = amount * price
+
+        if(isNaN(total)){
+            return alert("Por favor, digite o valor corretamente para converter.")
+        }
+
+        total = formatCurrencyBRL(total).replace("R$", "")
+
+        result.textContent = `${total} Reais`
+
+        footer.classList.add("show-result")
+
+    } catch(error){
+        console.log(error)
+        footer.classList.remove("show-result")
+        alert("Não foi possível converter. Tente novamente mais tarde.")
+    }
+
+}
+
+function formatCurrencyBRL(value){
+    return Number(value).toLocaleString("pt-BR", {
+        style: "currency",
+        currency: "BRL",
+    })
+}
